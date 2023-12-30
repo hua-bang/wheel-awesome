0b28fec384b6ed887667872683e269052d84df80
hua_bang <1964760211@qq.com>
2023-12-30 08:09:57 +0000

    feat(mini-bundler): createModule

---
+++ mini-bundler/src/index.js
@@ -0,0 +1,2 @@
+import axios from 'axios';
+const a = 2;
--- mini-bundler/src/index.ts
+++ mini-bundler/src/index.ts
@@ -1,126 +1,38 @@
-import { transformFromAstSync, traverse } from "@babel/core";
-import { Module } from "./typings";
-import * as fs from "fs";
-import { parse } from "@babel/parser";
-import * as path from "path";
+import * as fs from 'fs';
+import * as path from 'path';
+import * as parser from '@babel/parser';
+import traverse from '@babel/traverse';
 
-let id = 0;
-
-function getId() {
-  return id++;
+interface Module {
+  id: string;
+  filePath: string;
+  content: string;
+  dependencies: string[];
 }
 
-/**
- * 根据文件路径创建模块对象
- * @param filePath 文件路径
- * @returns 模块对象
- */
-const createModule = (filePath: string): Module => {
-  const dependencies: Module["dependencies"] = [];
+function createModule(filePath: string): Module {
+  const content = fs.readFileSync(filePath, 'utf-8');
 
-  const content = fs.readFileSync(filePath, "utf-8");
+  const dependencies: string[] = [];
 
-  const ast = parse(content, { sourceType: "module" });
-
-  const { code } =
-    transformFromAstSync(ast, content, {
-      presets: ["@babel/preset-env"],
-    }) || {};
+  const ast = parser.parse(content, {
+    sourceType: 'module',
+  });
 
   traverse(ast, {
-    ImportDeclaration: ({ node }) => {
+    ImportDeclaration({ node }) {
       dependencies.push(node.source.value);
-    },
+    }
   });
 
   return {
-    id: getId(),
-    code: code || "",
+    id: path.resolve(filePath),
     filePath,
-    dependencies,
-    mapping: {},
+    content,
+    dependencies
   };
-};
+}
 
-/**
- * 根据文件路径创建模块依赖图
- * @param filePath 文件路径
- * @returns 模块依赖图
- */
-const createDependencyGraph = (entry: string): Module[] => {
-  const entryModule = createModule(entry);
-  const graphArray: Module[] = [entryModule];
+const currModule = createModule('./index.js');
 
-  for (const module of graphArray) {
-    module.dependencies.forEach((dependencyPath) => {
-      const absolutePath = path.resolve(
-        path.dirname(entryModule.filePath),
-        dependencyPath
-      );
-      const childModule = createModule(absolutePath);
-      module.mapping[dependencyPath] = childModule.id;
-      graphArray.push(childModule);
-    });
-  }
-
-  return graphArray;
-};
-
-/**
- * 根据模块依赖图生成代码
- * @param graph 模块依赖图
- * @returns 生成代码
- */
-const bundle = (graph: Module[]) => {
-  let moduleStr = "";
-
-  graph.forEach((module) => {
-    moduleStr += `
-    ${module.id}: [
-      function(require, module, exports) {
-        ${module.code}
-      },
-      ${JSON.stringify(module.mapping)}
-    ],
-    `;
-  });
-
-  const result = `
-    (function(modules) {
-
-      let cache = {};
-
-      function require(id) {
-        if(cache[id]) {
-          return cache[id].exports;
-        }
-
-        const [fn, mapping] = modules[id];
-
-        const module = { exports: {} };
-
-        function localRequire(name) {
-          return require(mapping[name]);
-        }
-
-        fn(localRequire, module, module.exports)
-        cache[id] = module;
-
-        return module.exports;
-      }
-
-      require(0);
-
-    })({${moduleStr}})
-  `;
-
-  return result;
-};
-
-export const run = (filePath: string) => {
-  const moduleGraph = createDependencyGraph(filePath);
-
-  const result = bundle(moduleGraph);
-
-  fs.writeFileSync("./bundle.js", result);
-};
+console.log(currModule);
