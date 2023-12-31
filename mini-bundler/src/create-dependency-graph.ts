bf9ea0ac7eb557f0d3db9b8d431046a3b7234947
hua_bang <1964760211@qq.com>
2023-12-31 06:42:34 +0000

    feat: update file path

---
+++ mini-bundler/src/create-dependency-graph.ts
@@ -0,0 +1,38 @@
+import * as path from "path";
+import { createModule } from "./create-module";
+import { DependencyGraph, Module } from "./typings";
+
+/**
+ * 创建依赖关系图。
+ * 此函数根据入口文件路径构建项目的依赖关系图。它递归地遍历每个模块的依赖项，
+ * 并将它们添加到依赖关系图中，这个图是一个映射，键为模块ID，值为模块对象。
+ *
+ * @param {string} entry - 入口文件的路径。
+ * @returns {DependencyGraph} - 表示依赖关系的图，这是一个映射，其中键是模块ID，值是模块对象。
+ */
+export const createDependencyGraph = (entry: string): DependencyGraph => {
+  const entryModule = createModule(entry);
+
+  const graph: DependencyGraph = new Map<string, Module>();
+
+  const explore = (module: Module) => {
+    if (graph.has(module.id)) {
+      return;
+    }
+
+    graph.set(module.id, module);
+
+    module.dependencies.forEach((dependencyPath) => {
+      const absoluteDependencyPath = path.resolve(
+        module.filePath,
+        "..",
+        dependencyPath
+      );
+      const dependencyModule = createModule(absoluteDependencyPath);
+      explore(dependencyModule);
+    });
+  };
+
+  explore(entryModule);
+  return graph;
+};
---
+++ mini-bundler/src/create-module.ts
@@ -0,0 +1,62 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as parser from "@babel/parser";
+import * as BabelType from "@babel/types";
+import { transformFromAstSync, traverse } from "@babel/core";
+import { Module } from "./typings";
+
+export function createModule(filePath: string): Module {
+  const fileExtension = path.extname(filePath);
+  let content = "";
+
+  const dependencies: string[] = [];
+
+  const mapping: Record<string, string> = {};
+
+  if (fileExtension === ".js") {
+    content = fs.readFileSync(filePath, "utf-8");
+    const ast = parser.parse(content, {
+      sourceType: "module",
+    });
+
+    const { code } =
+      transformFromAstSync(ast, content, {
+        presets: ["@babel/preset-env"],
+      }) || {};
+
+    content = code || content;
+
+    traverse(ast, {
+      ImportDeclaration({ node }) {
+        dependencies.push(node.source.value);
+      },
+      CallExpression({ node }) {
+        if (
+          node.callee.type === "Identifier" &&
+          node.callee.name === "require"
+        ) {
+          if (BabelType.isStringLiteral(node.arguments[0])) {
+            dependencies.push(node.arguments[0].value);
+          }
+        }
+      },
+    });
+
+    dependencies.forEach((dependency) => {
+      const absoluteDependencyPath = path.resolve(filePath, "..", dependency);
+      mapping[dependency] = absoluteDependencyPath;
+    });
+  } else if (fileExtension === ".json") {
+    // 直接读取JSON文件并转换为JavaScript对象
+    const jsonContent = fs.readFileSync(filePath, "utf-8");
+    content = `module.exports = ${jsonContent}`;
+  }
+
+  return {
+    id: path.resolve(filePath),
+    filePath,
+    content,
+    dependencies,
+    mapping,
+  };
+}
