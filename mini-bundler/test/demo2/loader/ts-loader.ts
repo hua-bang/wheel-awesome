9ef19ed5283f9aab84ebf044f3578411495f03b6
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-01-03 22:22:45 +0800

    feat(mini-bundler): ts-loader support babel

--- mini-bundler/test/demo2/loader/ts-loader.ts
+++ mini-bundler/test/demo2/loader/ts-loader.ts
@@ -1,4 +1,5 @@
 import * as ts from "typescript";
+import * as babel from "@babel/core";
 import { Loader } from "../../../src/loader";
 
 export const tsLoader: Loader = (content, filePath) => {
@@ -11,3 +12,21 @@ export const tsLoader: Loader = (content, filePath) => {
   });
   return transpiled.outputText;
 };
+
+export const tsLoaderUserBabel: Loader = (content, filePath) => {
+  const res = babel.transformSync(content, {
+    // Babel 配置，您可以在这里指定预设、插件等
+    presets: ["@babel/preset-env", "@babel/preset-typescript"],
+    filename: filePath,
+  });
+
+  return res?.code || "";
+};
+
+export const getTSLoader = (options: TsLoaderOptions) => {
+  return options.useBabel ? tsLoaderUserBabel : tsLoader;
+};
+
+interface TsLoaderOptions {
+  useBabel?: boolean;
+}
