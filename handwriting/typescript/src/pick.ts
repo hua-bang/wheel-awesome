7ff3c99038efade4e4b78f13f197dc32b4d10634
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-06-17 23:14:58 +0800

    feat(ts): easy

---
+++ handwriting/typescript/src/pick.ts
@@ -0,0 +1,27 @@
+type MyPick<T, K extends keyof T> = {
+  [key in K]: T[key];
+};
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
+  MyPick<Todo, "title" | "completed" | "invalid">
+];
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+}
+
+interface Expected2 {
+  title: string;
+  completed: boolean;
+}
---
+++ handwriting/typescript/src/readonly.ts
@@ -0,0 +1,17 @@
+type MyReadonly<T> = {
+  readonly [key in keyof T]: T[key];
+};
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>];
+
+interface Todo1 {
+  title: string;
+  description: string;
+  completed: boolean;
+  meta: {
+    author: string;
+  };
+}
