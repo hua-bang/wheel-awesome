be3799447f35db7550d6e9a19d4b481bd5cbcc30
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-06-30 10:02:42 +0800

    feat: length of string

---
+++ handwriting/typescript/src/LengthOfString.ts
@@ -0,0 +1,35 @@
+/*
+  298 - Length of String
+  -------
+  by Pig Fang (@g-plane) #medium #template-literal
+
+  ### Question
+
+  Compute the length of a string literal, which behaves like `String#length`.
+
+  > View on GitHub: https://tsch.js.org/298
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type StrToArray<S extends string> = S extends `${infer S1}${infer S2}`
+  ? [1, ...StrToArray<S2>]
+  : [];
+type LengthOfString<S extends string> = StrToArray<S>["length"];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<LengthOfString<"">, 0>>,
+  Expect<Equal<LengthOfString<"kumiko">, 6>>,
+  Expect<Equal<LengthOfString<"reina">, 5>>,
+  Expect<Equal<LengthOfString<"Sound! Euphonium">, 16>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/298/answer
+  > View solutions: https://tsch.js.org/298/solutions
+  > More Challenges: https://tsch.js.org
+*/
