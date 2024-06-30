2a2c133210d517042804157e42db5fbc3a691712
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-06-30 17:59:35 +0800

    feat: react 16

--- react/react-16/src/App.tsx
+++ react/react-16/src/App.tsx
@@ -1,31 +1,73 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() => {
+    return 0;
+  });
+  const [count1, setCount1] = useState(1);
 
-	return (
-		<div className="App">
-			<div>
-				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Rspack + React + TypeScript</h1>
-			<div className="card">
-				<button onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Rspack and React logos to learn more
-			</p>
-		</div>
-	);
+  console.log("render count", count);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Rspack + React + TypeScript</h1>
+      <div className="card">
+        <button
+          onClick={() => {
+            Promise.resolve().then(() => {
+              console.log(`step: 第 ${count + 1} 次 promise`);
+            });
+            setCount((count) => {
+              console.log(`step: 第 ${count + 1} 次 set value`);
+              return count + 1;
+            });
+
+            setCount((count) => {
+              console.log(`step: 第 ${count + 1} 次 set value 2`);
+              return count + 1;
+            });
+
+            console.log(`step: 第 ${count + 1} 次 log`);
+          }}
+        >
+          count is {count}
+        </button>
+
+        <button
+          onClick={() => {
+            setTimeout(() => {
+              console.log("Step 1: setCount1 ");
+              setCount1((count) => {
+                debugger;
+                return count + 1;
+              });
+
+              console.log("Step 2: setCount1 ");
+              setCount1((count) => {
+                debugger;
+                return count + 1;
+              });
+            }, 200);
+          }}
+        >
+          count is {count1}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Rspack and React logos to learn more
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- react/react-16/src/main.tsx
+++ react/react-16/src/main.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
