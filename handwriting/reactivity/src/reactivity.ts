a3449798f26ef96faf5aa0bf5ebe0641a25ba8be
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-07-28 18:02:05 +0800

    feat: support object defineProperty

--- handwriting/reactivity/src/reactivity.ts
+++ handwriting/reactivity/src/reactivity.ts
@@ -1,6 +1,8 @@
 import { track, trigger } from "./effect";
 
-export const reactivity = (target: Record<string, any>): any => {
+const SUPPORT_PROXY = false;
+
+const reactivityByProxy = (target: Record<string, any>): any => {
   return new Proxy(target, {
     get(target, key) {
       track(target, key);
@@ -19,3 +21,42 @@ export const reactivity = (target: Record<string, any>): any => {
     },
   });
 };
+
+function reactivityByDefineProperty(target: Record<string, any>): any {
+  const handles = {
+    get(target: Record<string, any>, key: string | symbol) {
+      track(target, key);
+      const result = target[key as any];
+
+      if (typeof result === "object" && result !== null) {
+        return reactivityByDefineProperty(result);
+      }
+
+      return result;
+    },
+    set(target: Record<string, any>, key: string | symbol, value: any) {
+      target[key as any] = value;
+      trigger(target, key);
+      return true;
+    },
+  };
+
+  const reactivityObject = {};
+
+  Object.keys(target).forEach((key) => {
+    Object.defineProperty(reactivityObject, key, {
+      get() {
+        return handles.get(target, key);
+      },
+      set(value) {
+        return handles.set(target, key, value);
+      },
+    });
+  });
+
+  return reactivityObject;
+}
+
+export const reactivity = SUPPORT_PROXY
+  ? reactivityByProxy
+  : reactivityByDefineProperty;
