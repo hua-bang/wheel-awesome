35334ff990f95a15f67d651f7ef319ae2db4d1d6
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-08-08 22:39:26 +0800

    feat: ts-loader & babel loader

--- bundler/ts-check/webpack/src/index.js
+++
@@ -1,7 +0,0 @@
-function add(a, b) {
-  return a + b;
-}
-
-console.log(add(1, 2));
-
-export default add;
--- bundler/ts-check/webpack/webpack.config.js
+++ bundler/ts-check/webpack/webpack.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      // {
+      //   test: /\.tsx?$/,
+      //   use: "ts-loader",
+      //   exclude: /node_modules/,
+      // },
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
