c166e320a67da341abe1d6f9867994abaa37732e
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-08-08 22:57:56 +0800

    feat: swc-loader

--- bundler/ts-check/webpack/webpack.config.js
+++ bundler/ts-check/webpack/webpack.config.js
@@ -1,17 +1,40 @@
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.ts",
   module: {
     rules: [
+      // ts-loader
       // {
       //   test: /\.tsx?$/,
       //   use: "ts-loader",
       //   exclude: /node_modules/,
       // },
+      // babel-loader
       {
         test: /\.tsx?$/,
         use: "babel-loader",
         exclude: /node_modules/,
       },
+
+      // swc-loader
+      // {
+      //   test: /\.tsx?$/,
+      //   use: {
+      //     loader: "swc-loader",
+      //     options: {
+      //       jsc: {
+      //         parser: {
+      //           syntax: "typescript",
+      //           tsx: true,
+      //         },
+      //         target: "es5",
+      //       },
+      //     },
+      //   },
+      //   exclude: /node_modules/,
+      // },
     ],
   },
+  plugins: [new ForkTsCheckerWebpackPlugin()],
 };
