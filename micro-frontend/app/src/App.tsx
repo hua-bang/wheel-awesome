72b9d957a609a8340a30fafde866df699de77109
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-08-10 16:13:02 +0800

    feat: micro-frontend app

---
+++ micro-frontend/app/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Main App</h1>
+      <div className="card">Sub App</div>
+    </div>
+  );
+}
+
+export default App;
