172a76b5be44eebd02b6f98abd9d1dbe9930bccf
hua <huabang001124@gmail.com>
2024-09-09 01:50:04 +0000

    feat(handwriting): curring

--- handwriting/currying/index.ts
+++ handwriting/currying/index.ts
@@ -1,16 +1,16 @@
 const currying = <T extends (...args: any[]) => any>(fn: T) => {
-  const curryFn = (...args: any[]) => {
+  const curried = (...args: any[]) => {
     if (args.length >= fn.length) {
       return fn(...args);
     }
 
     return (...args2: any[]) => {
-      return curryFn(...args, ...args2);
-    };
-  };
+      return curried(...args, ...args2);
+    }
+  }
 
-  return curryFn;
-};
+  return curried;
+}
 
 const add = (a: number, b: number, c: number) => {
   return a + b + c;
